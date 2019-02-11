a4661fafbada2478dc8577c02a9b8af11b0eb092
ZlatKa <zlatka.zueva@gmail.com>
2019-02-12 00:42:02 +0300

    Lab2 done

---
+++ MaxMinAlgorithm/MaxMinAlgorithm/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MaxMinAlgorithm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Color[] colorsArr = new Color[] { Colors.MediumSeaGreen, Colors.SandyBrown, Colors.SlateBlue, Colors.DeepPink, Colors.GreenYellow,
+                                                  Colors.IndianRed, Colors.LightSkyBlue, Colors.Crimson, Colors.SpringGreen, Colors.LightPink,
+                                                  Colors.CornflowerBlue, Colors.Gold, Colors.Maroon, Colors.MidnightBlue, Colors.Orchid,
+                                                  Colors.Lime, Colors.Tan, Colors.Brown, Colors.MediumSlateBlue, Colors.Coral};
+        private static readonly Color kernelColor = Colors.Black;
+        private static readonly Color kernelBorderColor = Colors.Red;
+        private static readonly Color pointsBorderColor = Color.FromRgb(50, 50, 50);
+        private static readonly double kernelsRelativeSize = 0.015;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            int objects = (int)sliderObjectsAmount.Value;
+            ThreadPool.QueueUserWorkItem((obj) => { StartAlgorithm((SynchronizationContext)obj, objects); }, SynchronizationContext.Current);
+        }
+
+        private void ChangeElementsState(bool isEnabled)
+        {
+            btnStart.IsEnabled = isEnabled;
+            sliderObjectsAmount.IsEnabled = isEnabled;
+            txtClassesAmount.IsEnabled = isEnabled;
+            if (isEnabled)
+            {
+                this.ResizeMode = ResizeMode.CanResize;
+            }
+            else
+            {
+                this.ResizeMode = ResizeMode.NoResize;
+                txtClassesAmount.Text = "0";
+                DrawCanvas.Children.Clear();
+            }
+        }
+
+        private void StartAlgorithm(SynchronizationContext context, int objectsAmount)
+        {
+            context.Send((x) =>
+            {
+                ChangeElementsState(isEnabled: false);
+            }, null);
+
+            Point[] objects = GenerateRandomPoints(objectsAmount);
+
+            MaxMin<Point> maxMinAlgorithm = new MaxMin<Point>(objectsAmount);
+            List<int> kernelIndexes = maxMinAlgorithm.ChooseTwoKernels(objects,DistanceBetweenPoints);
+            Dictionary<Point, int> classesDivision = maxMinAlgorithm.DivideIntoClasses(objects, kernelIndexes, DistanceBetweenPoints);
+
+            Task drawingTask = new Task((obj) => { AddPointsAsync(obj, classesDivision, objects, kernelIndexes); }, context);
+            drawingTask.Start();
+
+            while (maxMinAlgorithm.TryFindNewKernel(classesDivision, objects, ref kernelIndexes, DistanceBetweenPoints))
+            {
+                drawingTask.Wait();
+                Thread.Sleep(1000);
+
+                classesDivision = maxMinAlgorithm.DivideIntoClasses(objects, kernelIndexes, DistanceBetweenPoints);
+
+                drawingTask = new Task((obj) => { RepaintPoints(obj, objects,classesDivision, kernelIndexes); }, context);
+                drawingTask.Start();
+            }
+            context.Send((x) =>
+            {
+                ((Ellipse)DrawCanvas.Children[DrawCanvas.Children.Count - 1]).Fill = new SolidColorBrush(kernelColor);
+                ChangeElementsState(isEnabled: true);
+            }, null);
+        }
+
+        private void AddPoint(double x, double y, double wh, Color pointColor, Color borderColor)
+        {
+            Ellipse point = new Ellipse();
+            point.Width = point.Height = wh;
+            point.Fill = new SolidColorBrush(pointColor);
+            point.Stroke = new SolidColorBrush(borderColor);
+            point.Margin = new Thickness(x - point.Width / 2, y - point.Height / 2, 0, 0);
+            DrawCanvas.Children.Add(point);
+        }
+
+        private void AddPointsAsync(object context, Dictionary<Point, int> classesDivision, Point[] points, List<int> kernelIndexes)
+        {
+            ((SynchronizationContext)context).Send((x) =>
+            {
+                DrawCanvas.Children.Clear();
+
+                int pointsAmount = points.Count();
+                double pointsWidth = Math.Sqrt(DrawCanvas.ActualWidth * DrawCanvas.ActualHeight / pointsAmount);
+                for (int i = 0; i < pointsAmount; i++)
+                {
+                    AddPoint(points[i].X, points[i].Y, pointsWidth, colorsArr[classesDivision[points[i]]], pointsBorderColor);
+                }
+            }, null);
+            ((SynchronizationContext)context).Send((x) =>
+            {
+                double kernelsWidth = DrawCanvas.ActualWidth * kernelsRelativeSize;
+                for (int i = 0; i < 2; i++)
+                {
+                    AddPoint(points[kernelIndexes[i]].X, points[kernelIndexes[i]].Y, kernelsWidth, kernelColor, kernelBorderColor);
+                }
+            }, null);
+        }
+
+        private void RepaintPoints(object context, Point[] points, Dictionary<Point, int> classesDivision, List<int> kernelIndexes)
+        {
+            int classesAmount = kernelIndexes.Count();
+            Point newKernel = points[kernelIndexes.Last()];
+            
+            List<int> newClassPointsIndexes = new List<int>();
+            for (int i=0;i<points.Count();i++)
+            {
+                if (classesDivision[points[i]] == classesAmount-1)
+                {
+                    newClassPointsIndexes.Add(i);
+                }
+            }
+            int classPointsAmount = newClassPointsIndexes.Count();
+            
+            double kernelsWidth = DrawCanvas.ActualWidth * kernelsRelativeSize;
+            ((SynchronizationContext)context).Send((x) =>
+            {
+                for (int i = 0; i < classPointsAmount; i++)
+                {
+                    ((Ellipse)DrawCanvas.Children[newClassPointsIndexes[i]]).Fill = new SolidColorBrush(colorsArr[classesAmount-1]);      
+                }
+                ((Ellipse)DrawCanvas.Children[DrawCanvas.Children.Count - 1]).Fill = new SolidColorBrush(kernelColor);
+                AddPoint(newKernel.X, newKernel.Y, kernelsWidth, Colors.Red, kernelBorderColor);
+                txtClassesAmount.Text = (classesAmount).ToString();
+            }, null);
+        }
+
+        public double DistanceBetweenPoints(Point p1, Point p2)
+        {
+            return Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
+        }
+
+        private Point[] GenerateRandomPoints(int amount)
+        {
+            Point[] points_arr = new Point[amount];
+            Random random = new Random();
+            int i = 0;
+            while (i < amount)
+            {
+                Point newPoint = new Point(random.Next((int)DrawCanvas.ActualWidth), random.Next((int)DrawCanvas.ActualHeight));
+                if ((amount > 10000) || !points_arr.Contains(newPoint))
+                {
+                    points_arr[i++] = newPoint;
+                }
+            }
+            return points_arr;
+        }
+
+        private void winMain_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawCanvas.Children.Clear();
+        }
+    }
+}
---
+++ MaxMinAlgorithm/MaxMinAlgorithm/MaxMin.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMinAlgorithm
+{
+    class MaxMin<TObject> where TObject:struct
+    {
+        static public readonly int MaxObjectsAmount = 100000;
+        static public readonly int MinObjectsAmount = 1000;
+
+        int _objectsAmount;
+        int _classesAmount;
+
+        public int ObjectsAmount
+        {
+            get
+            {
+                return _objectsAmount;
+            }
+            private set
+            {
+                if (value >= MinObjectsAmount && value <= MaxObjectsAmount)
+                {
+                    _objectsAmount = value;
+                }
+            }
+        }
+
+        public int ClassesAmount
+        {
+            get
+            {
+                return _classesAmount;
+            }
+            private set
+            {
+                _classesAmount = value;
+            }
+        }
+
+        public MaxMin(int objectsAmount)
+        {
+            this.ObjectsAmount = objectsAmount;
+        }
+
+        public List<int> ChooseTwoKernels (TObject[] objects, Func<TObject, TObject, double> Distance)
+        {
+            List<int> kernels = new List<int>();
+            if (objects.Count() == _objectsAmount)
+            {
+                Random random = new Random();
+                kernels.Add(random.Next(objects.Count()));
+                double maxDistance = 0;
+                int secondKernelIndex = 0;
+                for (int i=0; i<objects.Count();i++)
+                {
+                    double distance = Distance(objects[i], objects[kernels[0]]);
+                    if (distance>maxDistance)
+                    {
+                        maxDistance = distance;
+                        secondKernelIndex = i;
+                    }
+                }
+                kernels.Add(secondKernelIndex);
+            }
+            return kernels;
+        }
+
+        public Dictionary<TObject, int> DivideIntoClasses(TObject[] objects, List<int> kernelsIndexes, Func<TObject, TObject, double> Distance)
+        {
+            Dictionary<TObject, int> division = new Dictionary<TObject, int>(objects.Count());
+            if (objects.Count() == _objectsAmount)
+            {
+                Task<int>[] tasksArr = new Task<int>[_objectsAmount];
+                for (int i = 0; i < _objectsAmount; i++)
+                {
+                    tasksArr[i] = new Task<int>((objNum) =>
+                    {
+                        double minDistance = double.MaxValue;
+                        int classNum = 0;
+                        for (int j = 0; j < kernelsIndexes.Count(); j++)
+                        {
+                            double distance = Distance(objects[(int)objNum], objects[kernelsIndexes[j]]);
+                            if (distance < minDistance)
+                            {
+                                minDistance = distance;
+                                classNum = j;
+                            }
+                        }
+                        return classNum;
+                    }, i);
+                    tasksArr[i].Start();
+                }
+                for (int i = 0; i < _objectsAmount; i++)
+                {
+                    tasksArr[i].Wait();
+                    division[objects[i]] = tasksArr[i].Result;
+                }
+            }
+            return division;
+        }
+
+        private TObject FindClassMostRemote(List<TObject> classObjects, TObject kernel, Func<TObject, TObject, double> Distance)
+        {
+            TObject theMostRemote = new TObject();
+            if (classObjects.Count() > 0)
+            {
+                double maxDistance = 0;
+                for (int objectNum = 0; objectNum < classObjects.Count(); objectNum++)
+                {
+                    double distance = Distance(kernel, classObjects[objectNum]);
+                    if (distance > maxDistance)
+                    {
+                        maxDistance = distance;
+                        theMostRemote = classObjects[objectNum];
+                    }
+                } 
+            }
+            return theMostRemote;
+        }
+
+        public bool TryFindNewKernel (Dictionary<TObject, int> classesDictionary, TObject[] objects, ref List<int> kernelsIndexes, Func<TObject, TObject, double> Distance)
+        {
+            bool isFound = false;
+            if (objects.Count() == _objectsAmount)
+            {
+                _classesAmount = kernelsIndexes.Count();
+                Task<TObject>[] tasksArr = new Task<TObject>[_classesAmount];
+                List<TObject> kernels = new List<TObject>();
+                for (int classNum = 0; classNum < _classesAmount; classNum++)
+                {
+                    kernels.Add(objects[kernelsIndexes[classNum]]);
+                }
+
+                for (int classNum = 0; classNum < _classesAmount; classNum++)
+                {
+                    List<TObject> classObjects = new List<TObject>();
+                    for (int objectNum = 0; objectNum < _objectsAmount; objectNum++)
+                    {
+                        if (classesDictionary[objects[objectNum]] == classNum)
+                        {
+                            classObjects.Add(objects[objectNum]);
+                        }
+                    }
+                    tasksArr[classNum] = new Task<TObject>((obj) => { return FindClassMostRemote(classObjects, kernels[(int)obj], Distance); },classNum);
+                    tasksArr[classNum].Start();
+                }
+                TObject theMostRemote = new TObject();
+                double maxDistance = 0;
+                for (int classNum = 0; classNum < _classesAmount; classNum++)
+                {
+                    tasksArr[classNum].Wait();
+                    TObject newKernel = tasksArr[classNum].Result;
+                    double distance = Distance(newKernel, kernels[classNum]);
+                    if (distance>maxDistance)
+                    {
+                        maxDistance = distance;
+                        theMostRemote = newKernel;
+                    }
+                }
+                
+                if (maxDistance>(FindAverageDistance(kernels,Distance)/2))
+                {
+                    isFound = true;
+                    _classesAmount++;
+                    kernelsIndexes.Add(Array.IndexOf(objects, theMostRemote));
+                }
+            }
+            return isFound;
+        }
+
+        private double FindAverageDistance (List<TObject> kernels, Func<TObject, TObject, double> Distance)
+        {
+            double distance = 0;
+            int amount = 0;
+            for (int i=0; i<kernels.Count();i++)
+            {
+                for (int j = i + 1; j < kernels.Count(); j++)
+                {
+                    distance += Distance(kernels[i], kernels[j]);
+                    amount++;
+                }
+            }
+            return distance / amount;
+        }
+    }
+}
